2350bb37bedd52dfeb5ed23bb8c6421ccac625a4
komalapa <komalapa@mail.ru>
2020-03-20 17:33:04 +0400

    unrle works

--- script/rle.js
+++ script/rle.js
@@ -23,9 +23,36 @@ function rle(raw){
     return code;
 }
 function unrle(code){
-
+    //от исходного массива запоминаю символ и удаляю. Регуляркой ищу число у начала строки, если есть удаляю и столько раз запомненый символ пишу в выход, если нет один раз.
+    let curSimbol='';
+    let output='';
+    let regexpNmb = /^(\d+)/;
+    let regexpLtr = /^[a-zA-Z]/;
+    //console.log(code.match(regexpNmb)[0]);
+    let codeLnth=code.length;//будет меняться в цикле
+    while(code){
+        if (regexpLtr.test(code)){
+            curSimbol=code[0]
+            output=output+code[0];
+            code=code.slice(1);
+            //console.log("simbol"+output + " code " +code);
+        } else{
+            for(let j=1; j<code.match(regexpNmb)[0]; j++){
+                output=output+curSimbol; 
+            }
+            while (regexpNmb.test(code)){
+                code=code.slice(1);
+            }
+            //console.log("number"+output);
+        }       
+    }
+    return output; 
+    
 }
 
 let input="AAAAAAAAAAAABAAAAAAAAAAAABBBAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAA";
-input="rrrtttyyyr";
+//input="rrrtttyyyrr";
 console.log(input+" >>RLE>> "+rle(input));
+
+console.log(unrle(rle(input))+" - unRLE");
+
