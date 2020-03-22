b5ff9d828bea2687aac8f42dcd97d3f734d0ab60
komalapa <komalapa@mail.ru>
2020-03-22 15:41:20 +0400

    new simple function

--- script/task4.js
+++ script/task4.js
@@ -1,4 +1,5 @@
 "use strict"
+//1
 console.log("------ 1 ------");
 function convertToObJ(nmb){
     let outputObj = {};
@@ -14,6 +15,7 @@ console.log("convert 345: ", convertToObJ(345));
 console.log("convert 1345: ", convertToObJ(1345));
 console.log("convert \"тысяча\": ", convertToObJ("тысяча"));
 
+//2
 console.log("------ 2 ------");
 function showFruct(){
     console.log (`color: ${this.color}
@@ -31,7 +33,37 @@ function newFruct(name,color,form,isTasty){
 };
 let apple = newFruct('apple','green','sphere', true);
 apple.show();
-
+//3
 console.log("------ 3 ------");
 
-
+//"долг" простые числа
+console.log("---------- простые ----------");
+function simple(maxNmb=100){
+    if (isNaN(maxNmb)||maxNmb<0||maxNmb>1000){
+        return null;
+    }
+    let i=2; 
+    let simpleNmbs=[];
+    let simpleFlag=true;
+    while (i<=maxNmb){
+        simpleFlag=true;
+        for (let j=0; j<simpleNmbs.length; j++){
+            console.log(i + " % " + simpleNmbs[j] +" = "+ i%simpleNmbs[j]);
+            if (simpleNmbs[j]<=(i/2)){
+                if (i%simpleNmbs[j]==0){
+                    simpleFlag=false;
+                    break;
+                }            
+            }
+        }
+        if (simpleFlag){
+            simpleNmbs.push(i++);
+        } else {
+            i++;
+        }
+    }
+    return simpleNmbs;
+}
+console.log(simple());
+console.log("эталон: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97.");
+console.log(91%7);
