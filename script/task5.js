be2d349c0b286cfaa51b5fa74f7df0b1cf26f535
komalapa <komalapa@mail.ru>
2020-03-27 00:01:17 +0400

    students list

--- script/task5.js
+++ script/task5.js
@@ -1,10 +1,18 @@
 function youtube(){
     let videos= document.querySelectorAll('[id=video-title-link]');
     let links=[];
-    //for (let i in videos){
-    //    links[i]=videos[i].href;
-    //}
     videos.forEach(video => links.push(video.href));
     return links;
 }
 youtube();
+
+function students(){
+    let studentsRaw = document.getElementsByClassName("mates-list__mate-name-block");
+    let students=[];
+    for (let i in studentsRaw){
+        students[studentsRaw[i].text]=studentsRaw[i].href;
+    }
+    return students;
+}
+
+students();
