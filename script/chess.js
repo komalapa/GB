dbd6312c778c685a1984ace0b0b1e01a0f80c500
komalapa <komalapa@mail.ru>
2020-03-27 23:20:34 +0400

    format chess

--- script/chess.js
+++ script/chess.js
@@ -1,47 +1,48 @@
-function generateField(color1="white",color2="black"){
+function generateField(color1 = "white", color2 = "black") {
     //добавляет 8 div - строк в каждом 8 div клеток
     let field = document.getElementById("chess-field");
     //console.log(field);
-    for (let i=0;i<8;i++){
+    for (let i = 0; i < 8; i++) {
         let row = document.createElement("div");
         row.className = "row";
-        for(let j=0;j<8;j++){
+        for (let j = 0; j < 8; j++) {
             let cell = document.createElement("div");
-            cell.className="cell";
-            if (i%2==j%2){
-                cell.style=`background-color : ${color1}`;
+            cell.className = "cell";
+            if (i % 2 == j % 2) {
+                cell.style = `background-color : ${color1}`;
             } else {
-                cell.style=`background-color : ${color2}`; 
+                cell.style = `background-color : ${color2}`;
             }
-            
-            row.appendChild(cell);    
+
+            row.appendChild(cell);
         }
         field.appendChild(row);
     }
 
 }
-function createCarousel(colors=["red","orange","yellow","green","lightblue","blue", "violet", "white", "grey","black"]){
+
+function createCarousel(colors = ["red", "orange", "yellow", "green", "lightblue", "blue", "violet", "white", "grey", "black"]) {
     let carousel = document.getElementById("carousel");
     let choosen = document.createElement("div");
-    choosen.className="carousel-choosen";
+    choosen.className = "carousel-choosen";
     carousel.appendChild(choosen);
     let itemsList = document.createElement("ul");
-    itemsList.className="carousel-colors-list";
-    
-    for (let i=0; i<colors.length;i++){
+    itemsList.className = "carousel-colors-list";
+
+    for (let i = 0; i < colors.length; i++) {
         let item = document.createElement("li");
-        item.className="carousel-colors-list-item";
-        item.id=`color${i}`;
-        item.style=`background-color : ${colors[i]}`;
-        item.onclick = function(){
-            choosen.style=`background-color : ${colors[i]}`;
+        item.className = "carousel-colors-list-item";
+        item.id = `color${i}`;
+        item.style = `background-color : ${colors[i]}`;
+        item.onclick = function () {
+            choosen.style = `background-color : ${colors[i]}`;
         }
         itemsList.appendChild(item);
     }
     carousel.appendChild(itemsList);
 }
 
-window.onload = function() {
+window.onload = function () {
     createCarousel();
-    generateField("green","yellow");
+    generateField("green", "yellow");
 }
