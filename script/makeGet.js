ca7cb7e2902a5a5effdd75d800c15a0801c49875
komalapa <komalapa@mail.ru>
2020-04-24 00:04:15 +0400

    makeGet with promice works

--- script/makeGet.js
+++ script/makeGet.js
@@ -2,7 +2,7 @@ const url="https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api
 const requestType={
     goodsList: "/catalogData.json", cart: "/getBasket.json", add: "/addToBasket.json", delete: "/deleteFromBasket.json"
 }
-function makeGETRequest(url, callback) {
+async function makeGETRequest(url) {
     var xhr;
   
     if (window.XMLHttpRequest) {
@@ -10,18 +10,53 @@ function makeGETRequest(url, callback) {
     } else if (window.ActiveXObject) { 
       xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
-  
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        callback(xhr.responseText);
-      }
-    }
-  
-    xhr.open('GET', url, true);
-    xhr.send();
-  }
+    const promice = new Promise ((resolve,reject) =>
+        {
+                console.log("ready")
+                xhr.onreadystatechange = function () {
+                    console.log ("ready state ", xhr.readyState)
+                    if (xhr.readyState === 4)  {
+                        if (xhr.status==200){ 
+                            console.log("state200")
+                            resolve(xhr.responseText);}
+                        else {
+                        console.log("state!200")
+                        errorCB();
+                        }
+                    }
+                }               
+        }
+    );
+    console.log("open ",url)
+                xhr.open('GET', url, true);
+                console.log("send")
+                xhr.send();
+    
+    return promice;
 
-makeGETRequest(url+requestType["goodsList"], (data) =>  console.log("Goods List: ", data));
-makeGETRequest(url+requestType["cart"], (data) =>  console.log("Cart: ", data));
-makeGETRequest(url+requestType["add"], (data) =>  console.log("Add Result: ", data));
-makeGETRequest(url+requestType["delete"], (data) =>  console.log("Delete Result: ", data));
+  }
+function successCB(data){
+    console.log( data);
+}
+function errorCB(data){
+    console.log("Something wrong");
+}
+
+makeGETRequest(url+requestType["goodsList"]).then((data)=>successCB("goods list: "+data)).catch( ()=>errorCB())
+makeGETRequest(url+requestType["cart"]).then((data)=>successCB("cart: "+data)).catch( ()=>errorCB())
+makeGETRequest(url+requestType["add"]).then((data)=>successCB("add: "+data)).catch( ()=>errorCB())
+makeGETRequest(url+requestType["delete"]).then((data)=>successCB("delete: "+data)).catch( ()=>errorCB())
+
+// function testPromice(time){
+//     let flag = false;
+//     const promice = new Promise ((resolve,reject) =>{
+//         setTimeout(()=>{
+//             console.log("OK");
+//             flag=true;
+//             if (flag) {resolve("true")} 
+//             else {reject("false")}},time);
+        
+//     })
+//     return promice;
+// }
+// testPromice(3000).then((date)=>console.log("resolve ",date)).catch((date)=>console.log("reject ",date))
