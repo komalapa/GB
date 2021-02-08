6d8cdd95ce4aea74188fa27aed2ff59fceeeb9b5
komalapa <komalapa@gmail.com>
2021-02-08 21:55:04 +0400

    ДЗ№1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
 import React, {createElement} from 'react';
 import ReactDom from 'react-dom';
 
-const messages = [
-    {name: 'Ivan', content: 'hello', id:0},
-    {name: 'Oleg', content: 'hi', id:1},
-    {name: 'Lena', content: 'hello', id:2},
-]
+// const messages = [
+//     {name: 'Ivan', content: 'hello', id:0},
+//     {name: 'Oleg', content: 'hi', id:1},
+//     {name: 'Lena', content: 'hello', id:2},
+// ]
 
+const SendMessage = ({name, content,id, send}) => {
+  
+    return(
+    <input type="button" onClick={send} value="send" />
+    )
+}
 const Message=({name, content, id}) => <li ><strong>{name}</strong> : {content}</li>
-const MessageList = ({messages}) => (
-    <ul>
-        {messages.map( item => <Message {...item} key={item.id} />)}
-    </ul>
+const MessageList = () =>{ 
+    const [messages, setMessages] = React.useState( [
+        {name: 'Ivan', content: 'hello', id:0},
+        {name: 'Oleg', content: 'hi', id:1},
+        {name: 'Lena', content: 'hello', id:2},
+    ]);
+    const handleSend = () => {
+        let newMessages=messages;
+        newMessages.push({name: 'Hercule', content: 'use your grey cells', id: (messages.length+1)});
+        setMessages([...newMessages])
+
+        console.log(messages)
+    }  
+    console.log(messages)
+    return (
+    <>
+        <ul>
+            {messages.map( item => {console.log(item); return(<Message {...item} key={item.id} />)})}
+        </ul>
+        <SendMessage name="Hercule" content = "Use your gray cells" id = {messages.length +1} send = {handleSend}/>
+    </>
 )
+}
+
 
 const mountPoint = document.getElementById('root')
 
 ReactDom.render(
-    <MessageList messages={messages}/>,
+    <MessageList />,
     mountPoint
 )
 //Задачи: Добавить отправку сообщения
